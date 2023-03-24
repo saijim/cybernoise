b34d4c1fe34d10631ed81f28c8e0d2088c514670
Jan Sagemüller <jan@netrebel.de>
2023-03-24 11:42:17 +0100

    enable compress again

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
+import compress from "astro-compress";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://q314.de",
   output: "static",
-  // integrations: [image(), compress(), sitemap()],
-  integrations: [image(), sitemap()],
+  integrations: [image(), compress(), sitemap()],
 });
