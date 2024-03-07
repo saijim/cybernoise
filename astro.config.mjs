8b3f8bb8c841bd9028b1211406e3161492418bc5
Jan Sagemüller <jan@netrebel.de>
2024-03-07 19:06:15 +0100

    do not compress pngs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import sitemap from "@astrojs/sitemap";
-import compress from "astro-compress";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://cybernoise.de",
   output: "static",
-  integrations: [compress(), sitemap()],
+  integrations: [
+    (await import("astro-compress")).default({
+      Exclude: [".*png"],
+    }),
+    sitemap(),
+  ],
 });
